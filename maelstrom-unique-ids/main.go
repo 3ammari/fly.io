302ce704661806e53c40ef6c65e41d9c701f6e4d
3ammari <me@3ammari.io>
2023-10-05 14:35:57 +0300

    a lucky solution : ID-<random int>

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"strconv"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
 
 func main() {
-	log.Println("running...")
+	n := maelstrom.NewNode()
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = "ID-" + strconv.Itoa(rand.Int())
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
